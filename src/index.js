216e38fc1b0861d006896a9caf7edc82fb68a313
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-09-08 18:33:15 +0300

    feat: do task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  return n < 0 ? reverseNegativeNumber(n) : reversePositiveNumber(n);
+}
+
+function reverseNegativeNumber(n) {
+  return +reverseStringNumber(-n); 
+}
+function reversePositiveNumber(n) {
+  return +reverseStringNumber(n);
+}
+function reverseStringNumber(n) {
+  return reverseString(`${n}`);
+}
+function reverseString(s) {
+  return s.split('').reverse().join('');
 }
